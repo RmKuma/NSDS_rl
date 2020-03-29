8d2c2748a4eb91d5af324b992543705599cdcc0f
rmkuma <party1996115@gmail.com>
2020-03-30 04:52:33 +0900

    merge check

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ import tensorflow as tf
 
 if __name__ == "__main__":
     #env = [ lambda : NetwEnv(8, serverPort=(5556+i)) for i in range(16)]
-    env = [  lambda: NetwEnv(8, serverPort=5556)] + [lambda:  NetwEnv(8, serverPort=5557)]+ [lambda:  NetwEnv(8, serverPort=5558)]+ [lambda:  NetwEnv(8, serverPort=5559)]+ [lambda:  NetwEnv(8, serverPort=5560)]+ [lambda:  NetwEnv(8, serverPort=5561)]+ [lambda:  NetwEnv(8, serverPort=5562)]+ [lambda:  NetwEnv(8, serverPort=5563)]+ [lambda:  NetwEnv(8, serverPort=5564)]+ [lambda:  NetwEnv(8, serverPort=5565)]+ [lambda:  NetwEnv(8, serverPort=5566)]+ [lambda:  NetwEnv(8, serverPort=5567)]+ [lambda:  NetwEnv(8, serverPort=5568)]+ [lambda:  NetwEnv(8, serverPort=5569)]+ [lambda:  NetwEnv(8, serverPort=5570)]+ [lambda:  NetwEnv(8, serverPort=5571)]
+    #env = [  lambda: NetwEnv(8, serverPort=5556)] + [lambda:  NetwEnv(8, serverPort=5557)]+ [lambda:  NetwEnv(8, serverPort=5558)]+ [lambda:  NetwEnv(8, serverPort=5559)]+ [lambda:  NetwEnv(8, serverPort=5560)]+ [lambda:  NetwEnv(8, serverPort=5561)]+ [lambda:  NetwEnv(8, serverPort=5562)]+ [lambda:  NetwEnv(8, serverPort=5563)]+ [lambda:  NetwEnv(8, serverPort=5564)]+ [lambda:  NetwEnv(8, serverPort=5565)]+ [lambda:  NetwEnv(8, serverPort=5566)]+ [lambda:  NetwEnv(8, serverPort=5567)]+ [lambda:  NetwEnv(8, serverPort=5568)]+ [lambda:  NetwEnv(8, serverPort=5569)]+ [lambda:  NetwEnv(8, serverPort=5570)]+ [lambda:  NetwEnv(8, serverPort=5571)]
     #env = [lambda : NetwEnv(8, serverPort=5556)]
-    env = SubprocVecEnv(env)
-    agent = PPO2(MlpPolicy, env, tensorboard_log="./log", verbose=1, n_steps=64, policy_kwargs={"act_fun":tf.nn.relu})
+    #env = SubprocVecEnv(env)
+    #agent = PPO2(MlpPolicy, env, tensorboard_log="./log", verbose=1, n_steps=64, policy_kwargs={"act_fun":tf.nn.relu})
 
-    #env = NetwEnv(20, serverPort=5556)
-    #agent = SAC(LnMlpPolicy, env, buffer_size=100000, tensorboard_log='./log', verbose=1)
+    env = NetwEnv(16, serverPort=10002)
+    agent = SAC(LnMlpPolicy, env, buffer_size=2000000, tensorboard_log='./log', verbose=1)
     agent.learn(total_timesteps=10000000, log_interval=1)
 
-    agent.save('./model/PPO_model')
+    agent.save('./model/SAC_model')
