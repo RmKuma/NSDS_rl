bdcb4f37414aaa480709c4301077b1b58ea5ac67
rmkuma <party1996115@gmail.com>
2020-03-30 20:32:15 +0900

    PPO with trained model

--- main.py
+++ main.py
@@ -10,14 +10,13 @@ from stable_baselines.common.atari_wrappers import LazyFrames
 
 
 if __name__ == "__main__":
-    #env = [ lambda : NetwEnv(8, serverPort=(5556+i)) for i in range(16)]
-    #env = [  lambda: NetwEnv(8, serverPort=5556)] + [lambda:  NetwEnv(8, serverPort=5557)]+ [lambda:  NetwEnv(8, serverPort=5558)]+ [lambda:  NetwEnv(8, serverPort=5559)]+ [lambda:  NetwEnv(8, serverPort=5560)]+ [lambda:  NetwEnv(8, serverPort=5561)]+ [lambda:  NetwEnv(8, serverPort=5562)]+ [lambda:  NetwEnv(8, serverPort=5563)]+ [lambda:  NetwEnv(8, serverPort=5564)]+ [lambda:  NetwEnv(8, serverPort=5565)]+ [lambda:  NetwEnv(8, serverPort=5566)]+ [lambda:  NetwEnv(8, serverPort=5567)]+ [lambda:  NetwEnv(8, serverPort=5568)]+ [lambda:  NetwEnv(8, serverPort=5569)]+ [lambda:  NetwEnv(8, serverPort=5570)]+ [lambda:  NetwEnv(8, serverPort=5571)]
-    #env = [lambda : NetwEnv(8, serverPort=5556)]
+    #env = [ lambda : NetwEnv(num_of_data=8, serverPort=(10056+i)) for i in range(16)]
+    #env = [  lambda: NetwEnv(num_of_data=8, serverPort=10056,k=2)] + [lambda:  NetwEnv(num_of_data=8, serverPort=10057,k=2)]+ [lambda:  NetwEnv(num_of_data=8, serverPort=10058,k=2)]+ [lambda:  NetwEnv(num_of_data=8, serverPort=10059,k=2)]+ [lambda:  NetwEnv(num_of_data=8, serverPort=10060,k=2)]+ [lambda:  NetwEnv(num_of_data=8, serverPort=10061,k=2)]+ [lambda:  NetwEnv(num_of_data=8, serverPort=10062,k=2)]+ [lambda:  NetwEnv(num_of_data=8, serverPort=10063, k=2)]
     #env = SubprocVecEnv(env)
     #agent = PPO2(MlpPolicy, env, tensorboard_log="./log", verbose=1, n_steps=64, policy_kwargs={"act_fun":tf.nn.relu})
 
-    checkpoint_callback = CheckpointCallback(save_freq=10000, save_path='./model/', name_prefix='SAC_model')
-    env = NetwEnv(8, serverPort=5557)
+    checkpoint_callback = CheckpointCallback(save_freq=100000, save_path='./model/', name_prefix='SAC_model')
+    env = NetwEnv(num_of_data=8, serverPort=10055, k=2)
     #env = FrameStack(env, 4)
     agent = SAC(LnMlpPolicy, env, 1, buffer_size=100000, tensorboard_log='./log', verbose=1)
     agent.learn(total_timesteps=10000000, log_interval=1, callback=checkpoint_callback)
